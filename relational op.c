ba96d54369b0312f0447d6ee75598babffc47774
TejasviArora <yash.yashlko@gmail.com>
2022-06-08 10:55:33 +0530

    relational commit

---
+++ relational op.c
@@ -0,0 +1,24 @@
+#include <stdio.h>  
+#include <math.h>  
+int main ()  
+{  
+    int a = 5;  
+    int b = 10;  
+      
+    printf (" a == b : %d", (a == b));  
+    if (a == b)  
+        printf ("\n %d is equal to %d", a, b);  
+    else  
+        printf (" \n %d is not equal to %d", a, b);  
+  
+      
+    int x = 5;  
+    int y = 5;  
+      
+    printf (" \n x == y : %d", (x == y));  
+    if (a == b)  
+        printf (" \n %d is equal to %d", x, y);  
+    else  
+        printf ("\n %d is not equal to %d", x, y);  
+    return 0;     
+} 
